cb15ad8185c2db713c8a12cc996227a0a35b3aca
Dan <33081566+deobfuscate@users.noreply.github.com>
2020-09-23 23:19:27 -0700

    Refactor and new files

--- includes/mysqli.php
+++ includes/mysqli.php
@@ -1,8 +1,5 @@
 <?php
-	DEFINE ('DB_HOST', 'localhost');
-	DEFINE ('DB_USER', '');
-	DEFINE ('DB_PASSWORD', '');
-	DEFINE ('DB_NAME', 'usersys');
+	include(dirname(__DIR__) . "..\..\incl.php");
 	
 	if ($dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)) {
 		//print '<p>Successfully connected!</p>';
--- login.php
+++ login.php
@@ -8,20 +8,21 @@
 	
 		$sql = "SELECT * FROM `users` WHERE `username` LIKE '$u' LIMIT 1";
 		$r = mysqli_query($dbc, $sql);
-		if (mysqli_affected_rows($dbc) == 0) {
+		if (!$r) {
+			$msg = "Error: " . mysqli_error($dbc);
+		}
+		elseif (mysqli_affected_rows($dbc) == 0) {
 			$msg = "Username does not exist.";
 		}
-		else {
-			while ($row = mysqli_fetch_array($r, MYSQL_ASSOC)) {
-				if (strtolower($u) == strtolower($row['username']) && md5($p) == $row['password']) {
-					$token = md5(strtolower($row['username']).":".$row['password']);
-					setcookie("username", $u, time() + (86400 * 30), "/");
-					setcookie("token", $token, time() + (86400 * 30), "/");
-					header("Location: index.php");
-				}
-				else {
-					$msg = "Username and password do not match.";
-				}
+		else while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+			if (strtolower($u) == strtolower($row['username']) && md5($p) == $row['password']) {
+				$token = md5(strtolower($row['username']).":".$row['password']);
+				setcookie("username", $u, time() + (86400 * 30), "/");
+				setcookie("token", $token, time() + (86400 * 30), "/");
+				header("Location: index.php");
+			}
+			else {
+				$msg = "Username and password do not match.";
 			}
 		}
 		mysqli_close($dbc);
@@ -38,9 +39,9 @@
 	<h1>Login</h1>
 	<?php
 		if(isset($msg)) { 
-				print "<p class=\"error\">".$msg."</p>\n";
+			print "<p class=\"error\">".$msg."</p>\n";
 		}
-  ?>
+	?>
   <form action="<?php print $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" name="login">
     <p><input name="username" type="text" size="32" maxlength="32" id="user" placeholder="Username" autofocus></p>
     <p><input name="password" type="password" size="32" maxlength="32" placeholder="Password"></p>
