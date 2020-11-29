6770fc9f5769c528dc86cdbf7b20cdf8401c759e
Dan <33081566+deobfuscate@users.noreply.github.com>
2020-11-28 22:20:51 -0800

    Use title setting

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,15 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title><?php print TITLE; ?></title>
+		<title><?php
+	$sql = "SELECT value FROM settings WHERE setting = 'forum_name'";
+	$q = mysqli_query($dbc, $sql);
+	$r = mysqli_fetch_array($q, MYSQLI_ASSOC);
+	if (mysqli_affected_rows($dbc) > 0)
+		print $r["value"]." - ".TITLE;
+	else
+		print TITLE;
+?></title>
 		<link href="assets/css/styles.css" rel="stylesheet" type="text/css">
 		<script src="assets/js/jquery.js"></script>
 		<script src="assets/js/register.js"></script>
