0320beaf6581d157a75e1a160e4214fb6309cf38
Dan <33081566+deobfuscate@users.noreply.github.com>
2020-12-17 23:16:19 -0800

    Userlist functionality

--- userlist.php
+++ userlist.php
@@ -15,5 +15,15 @@
 		<br>
 		<h2>User List</h2>
 <?php
+	$sql = "SELECT id, username, joined FROM users ORDER BY id ASC";
+	$q = mysqli_query($dbc, $sql);
+	if (mysqli_affected_rows($dbc) > 0)
+		while ($r = mysqli_fetch_array($q, MYSQLI_ASSOC)) {
+			print "		<p><a href=\"user.php?id={$r["id"]}\">{$r["username"]}</a><br><i>Joined {$r["joined"]}</i></p>\n";
+		}
+	else {
+		print "		<p>No users to display.</p>\n";
+	}
+
 	include("includes/footer.php");
 ?>
