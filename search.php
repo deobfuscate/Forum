02448138e8e58b6f274c5741a3a2911b60fa7a1d
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-01-23 23:03:06 -0800

    Implement basic search

--- search.php
+++ search.php
@@ -17,18 +17,26 @@
 <?php
 	if (isset($_POST['search']) && !empty($_POST['search'])) {
 		$search = htmlspecialchars($_POST['search']);
-		$query = "";
+		$where = htmlspecialchars($_POST['where']);
+		if ($where != "posts") $where = "topics"; // default to topics
+		$search = "%$search%";
+		$query = "SELECT * FROM $where INNER JOIN users ON $where.author = users.id WHERE body LIKE ?";
 		$stmt = $dbc->prepare($query);
-		$stmt->bind_param("s", $forum_name);
-		if ($stmt->execute())
-			;
-		else
-			print "		<p>Failed to search.</p>\n";
+		$stmt->bind_param("s", $search);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if ($stmt->affected_rows > 0)
+			while ($r = $result->fetch_assoc()) {
+				print "		<p>{$r["body"]}<br><i>Posted by {$r["username"]} at {$r["time"]}</i></p>\n";
+			}
+		else {
+			print "		<p>Could not find any results.</p>\n";
+		}
 	}
 ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data" name="register">
 			<p>Search for:<br><input name="search" type="text" size="32" autofocus></p>
-			<p>In: <input name="where" type="radio" value="topics">Topics</input> <input name="where" type="radio" value="posts">Posts</input></p>
+			<p>In: <input name="where" type="radio" value="topics" checked>Topics</input> <input name="where" type="radio" value="posts">Posts</input></p>
 			<p><button>Search</button></p>
 		</form>
 <?php
