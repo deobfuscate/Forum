6fbc138b66c64ec45f51b4a2b6fe5455096a4a20
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-06-28 22:48:34 -0700

    Max topic implementation

--- forum.php
+++ forum.php
@@ -18,7 +18,14 @@
 		<h2>Topics</h2>
 		<p><a href="post.php?cat=<?=$forum?>">Post a new topic</a></p>
 <?php
-	$query = "SELECT topics.id, topics.title, topics.time, users.username FROM topics INNER JOIN users ON topics.author = users.id WHERE topics.category = ? ORDER BY time ASC";
+	$sql = "SELECT value FROM settings WHERE setting = 'max_topics'";
+	$q = mysqli_query($dbc, $sql);
+	$r = mysqli_fetch_array($q, MYSQLI_ASSOC);
+	if (mysqli_affected_rows($dbc) > 0)
+		$max_topics = $r["value"];
+
+
+	$query = "SELECT topics.id, topics.title, topics.time, users.username FROM topics INNER JOIN users ON topics.author = users.id WHERE topics.category = ? ORDER BY time ASC LIMIT {$max_topics}";
 	$stmt = $dbc->prepare($query);
 	$stmt->bind_param("s", $forum);
 	$stmt->execute();
