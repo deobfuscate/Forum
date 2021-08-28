8c5411a5efaa6c7c4beee21327b8293fe3af5707
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-08-27 21:49:05 -0700

    Fix MySQLi logic

--- includes/mysqli.php
+++ includes/mysqli.php
@@ -1,8 +1,6 @@
 <?php
 	include(dirname(__DIR__) . "..\..\incl.php");
 	
-	if ($dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
-		//print '<p>Successfully connected!</p>';
-	else
+	if (!($dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)))
 		die('<p style="color: red;">Could not connect to MySQL:<br>'.mysqli_connect_error().'</p>');
 ?>
