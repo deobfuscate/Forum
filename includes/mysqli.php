5e415cc587df45732efd01b7baaeda78efe03ae4
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-08-28 21:22:35 -0700

    Update mysqli.php

--- includes/mysqli.php
+++ includes/mysqli.php
@@ -2,5 +2,5 @@
 	include(dirname(__DIR__) . "..\..\incl.php");
 	
 	if (!($dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)))
-		die('<p style="color: red;">Could not connect to MySQL:<br>'.mysqli_connect_error().'</p>');
+		die("<p style=\"color: red;\">Could not connect to MySQL:<br>" . mysqli_connect_error() . "</p>");
 ?>
