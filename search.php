60a2ace4fe898c57d9bfaf0fb614c73bbcd396c2
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-09-18 18:29:03 -0700

    Formatting

--- search.php
+++ search.php
@@ -5,7 +5,8 @@
 	if (isset($_POST['search']) && !empty($_POST['search'])) {
 		$search = htmlspecialchars($_POST['search']);
 		$where = htmlspecialchars($_POST['where']);
-		if ($where != "posts") $where = "topics"; // default to topics
+		if ($where != "posts")
+			$where = "topics"; // default to topics
 		$search = "%$search%";
 		$query = "SELECT * FROM $where INNER JOIN users ON $where.author = users.id WHERE body LIKE ?";
 		$stmt = $dbc->prepare($query);
@@ -14,13 +15,11 @@
 		$result = $stmt->get_result();
 		if ($stmt->affected_rows > 0) {
 			print "		<p>Found {$stmt->affected_rows} results:</p>\n";
-			while ($r = $result->fetch_assoc()) {
+			while ($r = $result->fetch_assoc())
 				print "		<p>{$r["body"]}<br><i>Posted by {$r["username"]} at {$r["time"]}</i></p>\n";
-			}
 		}
-		else {
+		else
 			print "		<p>Could not find any results.</p>\n";
-		}
 	}
 ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data" name="register">
