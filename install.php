000adb6a4d3ec7c9f2274faefb3e1e50b9618dcf
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-06-21 17:52:04 -0700

    Restore install.php

---
+++ install.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Install Forum</title>
+		<link href="assets/css/styles.css" rel="stylesheet" type="text/css">
+		<script src="assets/js/jquery.js"></script>
+	</head>
+	<body>
+		<h1>Forum</h1>
+		<h2>Install</h2>
+		<form action="<?=$_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data" name="install">
+			<p><input name="" type="text" size="32" maxlength="32" id="" placeholder="" autofocus></p>
+		</form>
+    </body>
+</html>
